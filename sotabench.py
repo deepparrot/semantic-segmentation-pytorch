d31694e000d152db8a9b2f59f7afc5631be7bfb3
deepparrot <53057954+deepparrot@users.noreply.github.com>
2019-09-27 00:38:35 +0100

    Create sotabench.py

---
+++ sotabench.py
@@ -0,0 +1,157 @@
+# System libs
+import os
+import time
+import argparse
+from distutils.version import LooseVersion
+# Numerical libs
+import numpy as np
+import torch
+import torch.nn as nn
+from scipy.io import loadmat
+# Our libs
+from config import cfg
+from dataset import ValDataset
+from models import ModelBuilder, SegmentationModule
+from utils import AverageMeter, colorEncode, accuracy, intersectionAndUnion, setup_logger
+from lib.nn import user_scattered_collate, async_copy_to
+from lib.utils import as_numpy
+from PIL import Image
+from tqdm import tqdm
+
+colors = loadmat('data/color150.mat')['colors']
+
+
+def visualize_result(data, pred, dir_result):
+    (img, seg, info) = data
+
+    # segmentation
+    seg_color = colorEncode(seg, colors)
+
+    # prediction
+    pred_color = colorEncode(pred, colors)
+
+    # aggregate images and save
+    im_vis = np.concatenate((img, seg_color, pred_color),
+                            axis=1).astype(np.uint8)
+
+    img_name = info.split('/')[-1]
+    Image.fromarray(im_vis).save(os.path.join(dir_result, img_name.replace('.jpg', '.png')))
+
+
+def evaluate(segmentation_module, loader, cfg, gpu):
+    acc_meter = AverageMeter()
+    intersection_meter = AverageMeter()
+    union_meter = AverageMeter()
+    time_meter = AverageMeter()
+    evaluator = ADE20KEvaluator(model_name='HRNetV2 (HRNetV2-W48)', paper_arxiv_id='1904.04514')
+
+    segmentation_module.eval()
+
+    pbar = tqdm(total=len(loader))
+    for batch_data in loader:
+        # process data
+        batch_data = batch_data[0]
+        seg_label = as_numpy(batch_data['seg_label'][0])
+        img_resized_list = batch_data['img_data']
+
+        torch.cuda.synchronize()
+        tic = time.perf_counter()
+        with torch.no_grad():
+            segSize = (seg_label.shape[0], seg_label.shape[1])
+            scores = torch.zeros(1, cfg.DATASET.num_class, segSize[0], segSize[1])
+            scores = async_copy_to(scores, gpu)
+
+            for img in img_resized_list:
+                feed_dict = batch_data.copy()
+                feed_dict['img_data'] = img
+                del feed_dict['img_ori']
+                del feed_dict['info']
+                feed_dict = async_copy_to(feed_dict, gpu)
+
+                # forward pass
+                scores_tmp = segmentation_module(feed_dict, segSize=segSize)
+                scores = scores + scores_tmp / len(cfg.DATASET.imgSizes)
+
+            _, pred = torch.max(scores, dim=1)
+            pred = as_numpy(pred.squeeze(0).cpu())
+
+        torch.cuda.synchronize()
+        time_meter.update(time.perf_counter() - tic)
+
+        # calculate accuracy
+        acc, pix = accuracy(pred, seg_label)
+        intersection, union = intersectionAndUnion(pred, seg_label, cfg.DATASET.num_class)
+        acc_meter.update(acc, pix)
+        intersection_meter.update(intersection)
+        union_meter.update(union)
+        
+        evaluator.update(output=pred.flatten().cpu().numpy(), target=seg_label.flatten().cpu().numpy())
+        
+        if evaluator.cache_exists:
+          break
+        
+        pbar.update(1)
+
+assert LooseVersion(torch.__version__) >= LooseVersion('0.4.0'), \
+    'PyTorch>=0.4.0 is required'
+
+cfg_str = 'ade20k-hrnetv2.yaml'
+gpu = 0
+
+cfg.merge_from_file(cfg_str)
+cfg['root_dataset'] = './.data/vision/ade20k/ADEChallengeData2016'
+cfg['list_train'] = "./.data/vision/ade20k/training.odgt"
+cfg['list_val'] = "./.data/vision/ade20k/validation.odgt"
+
+BATCH_SIZE = cfg.VAL.batch_size
+BATCH_SIZE = 32
+
+# absolute paths of model weights
+cfg.MODEL.weights_encoder = os.path.join(
+    cfg.DIR, 'encoder_' + cfg.VAL.checkpoint)
+cfg.MODEL.weights_decoder = os.path.join(
+    cfg.DIR, 'decoder_' + cfg.VAL.checkpoint)
+assert os.path.exists(cfg.MODEL.weights_encoder) and \
+    os.path.exists(cfg.MODEL.weights_decoder), "checkpoint does not exitst!"
+
+if not os.path.isdir(os.path.join(cfg.DIR, "result")):
+    os.makedirs(os.path.join(cfg.DIR, "result"))
+
+torch.cuda.set_device(gpu)
+
+# Network Builders
+net_encoder = ModelBuilder.build_encoder(
+    arch=cfg.MODEL.arch_encoder.lower(),
+    fc_dim=cfg.MODEL.fc_dim,
+    weights=cfg.MODEL.weights_encoder)
+net_decoder = ModelBuilder.build_decoder(
+    arch=cfg.MODEL.arch_decoder.lower(),
+    fc_dim=cfg.MODEL.fc_dim,
+    num_class=cfg.DATASET.num_class,
+    weights=cfg.MODEL.weights_decoder,
+    use_softmax=True)
+
+crit = nn.NLLLoss(ignore_index=-1)
+
+segmentation_module = SegmentationModule(net_encoder, net_decoder, crit)
+
+# Dataset and Loader
+dataset_val = ValDataset(
+    cfg.DATASET.root_dataset,
+    cfg.DATASET.list_val,
+    cfg.DATASET)
+loader_val = torch.utils.data.DataLoader(
+    dataset_val,
+    batch_size=BATCH_SIZE,
+    shuffle=False,
+    collate_fn=user_scattered_collate,
+    num_workers=5,
+    drop_last=True)
+
+segmentation_module.cuda()
+
+# Main loop
+evaluate(segmentation_module, loader_val, cfg, gpu)
+
+print('Evaluation Done!')
+
