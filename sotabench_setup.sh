7793ab888658627caab546c18163cf24c6cc4023
deepparrot <53057954+deepparrot@users.noreply.github.com>
2019-09-27 16:22:10 +0100

    Update sotabench_setup.sh

--- sotabench_setup.sh
+++ sotabench_setup.sh
@@ -1,3 +1,8 @@
+echo "Moving files"
+mv ./data/validation.odgt ./.data/vision/ade20k/validation.odgt
+mv ./data/training.odgt ./.data/vision/ade20k/training.odgt
+echo "Files moved"
+
 apt-get install unzip
 unzip ./.data/vision/ade20k/ADEChallengeData2016.zip -d ./.data/vision/ade20k/
 echo "Dataset downloaded."
